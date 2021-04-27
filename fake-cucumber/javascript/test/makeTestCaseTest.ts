7b2a21d56f3507b949dc9d80be1dffe2e8589965
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-04-27 15:00:05 +0200

    Update FakeCucumber to generate expected messages with empty scenarios

--- fake-cucumber/javascript/test/makeTestCaseTest.ts
+++ fake-cucumber/javascript/test/makeTestCaseTest.ts
@@ -37,7 +37,7 @@ describe('makeTestCase', () => {
   })
 
   context('when the pickle has no steps', () => {
-    it('generates a synthetic undefined test step', async () => {
+    it('generates a testCase without any steps', async () => {
       // See https://github.com/cucumber/cucumber/issues/249
       const pickle = new messages.Pickle({
         id: 'some-id',
@@ -61,7 +61,7 @@ describe('makeTestCase', () => {
       const messageList: messages.IEnvelope[] = []
       const listener: EnvelopeListener = (message: messages.IEnvelope) => messageList.push(message)
       await testCase.execute(listener, 0, 'some-test-case-started-id')
-      assert.equal(messageList.length, 4)
+      assert.strictEqual(messageList.length, 2)
     })
   })
 
